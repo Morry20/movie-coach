1e2ae780386f18389046b647bf77a15ee2ebbd12
Morry20 <ku.l.taf54@gmail.com>
2021-12-19 11:24:28 +0000

    [finish]サインインログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :sport_id, :image, :gender, :age, :introduction, :is_coach])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:email, :name, :sport_id, :image, :gender, :age, :introduction, :is_coach])
   end
 end
