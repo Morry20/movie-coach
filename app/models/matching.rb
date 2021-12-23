efe0f1b6450380474465da22b8fa8bc141513bf7
Morry20 <ku.l.taf54@gmail.com>
2021-12-23 04:47:27 +0000

    [finish]コーチング申請一覧画面

--- app/models/matching.rb
+++ app/models/matching.rb
@@ -1,8 +1,11 @@
 class Matching < ApplicationRecord
   #has_one :coach_evaluation, dependent: :destroy
-  
+
   #has_many :messages, dependent: :destroy
   #has_many :movies
-  
+
   belongs_to :user
+
+  enum approval: {checking:0,  approved: 1, impossible: 2}
+
 end
