b1a309abd009590e970f281f715d59be30c68059
Morry20 <ku.l.taf54@gmail.com>
2022-06-28 11:21:06 +0000

    [Fix] Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Sport.create(image: File.open("./app/assets/images/tennis2.jpg"), name: 'テニス')
-Sport.create(image: File.open("./app/assets/images/golf.jpg"), name: 'ゴルフ')
-Sport.create(image: File.open("./app/assets/images/soccer.jpg"), name: 'サッカー')
-Sport.create(image: File.open("./app/assets/images/baseball.jpg"), name: '野球')
+# Sport.create(image: File.open("./app/assets/images/tennis2.jpg"), name: 'テニス')
+# Sport.create(image: File.open("./app/assets/images/golf.jpg"), name: 'ゴルフ')
+# Sport.create(image: File.open("./app/assets/images/soccer.jpg"), name: 'サッカー')
+# Sport.create(image: File.open("./app/assets/images/baseball.jpg"), name: '野球')
+
+sports = [
+    {name: 'テニス', image: './app/assets/images/tennis2.jpg'},
+    {name: 'ゴルフ', image: './app/assets/images/golf.jpg'},
+    {name: 'サッカー', image: './app/assets/images/soccer.jpg'},
+    {name: '野球', image: './app/assets/images/baseball.jpg'},
+  ]
+
+sports.each do |sport|
+  sport_data = Sport.find_by(name: sport[:name])
+  if sport_data.nil?
+    Sport.create(
+        name: sport[:name],
+        image: File.open(sport[:image])
+      )
+  end
+end
